699ea7086d58bb0d848c78f8dee41f1a0b1b2f57
jerry582162 <582162@qq.com>
2022-05-25 15:24:15 +0800

    数据结构与算法练习

---
+++ algorithms/src/Print8/Print8.java
@@ -0,0 +1,24 @@
+package Print8;
+
+public class Print8 {
+    public static void print(int num) {
+        for (int i = 31; i >= 0; i--) {
+            System.out.print((num & (1 << i)) == 0 ? "0" : "1");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int num = 5;
+        print(num);
+        int tt = 5;
+        print(tt<<1);//左移一位 等同于*2
+        print(~tt);
+        print(~tt+1);//负数等于 取反加1
+        System.out.println(~tt+1);
+        int a = Integer.MAX_VALUE;
+        System.out.println(a);
+        int b = Integer.MIN_VALUE;
+        System.out.println(b);
+    }
+}
+
